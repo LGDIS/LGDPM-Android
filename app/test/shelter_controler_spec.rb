b7c563d9b0d4c38b469ab7e5557b36c263d37763
katamoto <katamoto.atsushi@adniss.jp>
2013-02-10 22:57:33 +0900

    ShelterControllerテスト

---
+++ app/test/shelter_controler_spec.rb
@@ -0,0 +1,53 @@
+require 'Shelter/shelter_controller'
+require 'spec_helper'
+
+describe "ShelterController" do
+  before(:all) do
+    @application = AppApplication.new
+    @controller = ShelterController.new
+  end
+
+  describe "download" do
+    before(:all) do
+      @lgdpm_http_server_authentication = Rho::RhoConfig.lgdpm_http_server_authentication
+      @lgdpm_shelter_download_url = Rho::RhoConfig.lgdpm_shelter_download_url
+      Rho::RhoConfig.lgdpm_http_server_authentication = ""
+      Rho::RhoConfig.lgdpm_shelter_download_url = "downloadURL"
+    end
+
+    it "ダウンロードファイルメソッドが呼び出されること" do
+      args = {:url => "downloadURL",
+      :filename => File.join(Rho::RhoApplication::get_model_path('app','Shelter'), 'new_shelter.json'),
+      :headers => {},
+      :callback => "/app/Shelter/download_callback",
+      :callback_param => ""}
+      Rho::AsyncHttp.should_receive(:download_file).with(args)
+      @controller.serve(@application, nil, SpecHelper.create_request("GET /Shelter/download"), {})
+    end
+    
+    after(:all) do
+      Rho::RhoConfig.lgdpm_http_server_authentication = @lgdpm_http_server_authentication
+      Rho::RhoConfig.lgdpm_shelter_download_url = @lgdpm_shelter_download_url
+    end
+  end
+  
+  describe "download_callback" do
+    context "ダウンロード正常終了の場合" do
+      it "避難所マスタがロードされ、メニュー画面に遷移すること" do
+        Shelter.should_receive(:load_shelter)
+        Alert.should_receive(:show_popup).with("避難所マスタのダウンロードが完了しました")
+        WebView.should_receive(:navigate).with(Rho::RhoConfig.start_path)
+        @controller.serve(@application, nil, SpecHelper.create_request("GET /Shelter/download_callback", "status" => "ok", "rho_callback" => true), {})
+      end
+    end
+    context "ダウンロードエラーの場合" do
+      it "エラー画面に遷移すること" do
+        WebView.should_receive(:navigate).with("/app/Shelter/show_error")
+        @controller.serve(@application, nil, SpecHelper.create_request("GET /Shelter/download_callback", "status" => "ng", "rho_callback" => true), {})
+      end
+      it "ファイルが削除されていること" do
+        File.exist?(File.join(Rho::RhoApplication::get_model_path('app','Shelter'), 'new_shelter.json')).should be_false
+      end
+    end
+  end
+end
