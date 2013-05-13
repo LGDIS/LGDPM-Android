8fd6faab1c78ebdf9b42bd261bffb9861ec177cb
atsutsumi <atsutsumi@di-system.co.jp>
2013-05-13 20:55:23 +0900

    year tag updated to display japanese calendar

--- app/helpers/browser_helper.rb
+++ app/helpers/browser_helper.rb
@@ -20,6 +20,24 @@ module BrowserHelper
     platform == 'blackberry' && (System::get_property('os_version').split('.')[0].to_i >= 6)
   end
   
+  def to_jcalendar(year)
+    if year < 1912
+      gg = '明治'
+      yy = year - 1867
+    elsif year >= 1912 && year <= 1926
+      gg = '大正'
+      yy = year - 1911
+    elsif year > 1926 && year < 1989
+      gg = '昭和'
+      yy = year - 1925
+    elsif year >= 1989
+      gg = '平成'
+      yy = year - 1988
+    end
+    
+    return gg + yy.to_s + '年'
+  end
+  
   # 年選択セレクトボックスタグのHTMLを返します
   # ==== Args
   # _options_ :: オプション
@@ -27,6 +45,28 @@ module BrowserHelper
   # 年選択セレクトボックスタグのHTML
   # ==== Raise
   def select_year_tag(options={})
+    tag = %(<select style="width:150px" data-mini="true")
+    tag << %( name="#{options[:name]}") if options[:name]
+    tag << %( id="#{options[:id]}") if options[:id]
+    tag << %( data-theme="#{options[:data_theme]}") if options[:data_theme]
+    tag << ">"
+    tag << %(<option value="">－</option>)
+    year = Time.now.year
+    Rho::RhoConfig.lgdpm_max_year.to_i.times do |x|
+      tag << %(<option value="#{year - x}" )
+      tag << selected(year - x, options[:value])
+      tag << %(>#{year - x }</option>)
+    end
+    tag << %(</select>)
+  end
+  
+  # 年選択セレクトボックスタグのHTMLを返します
+  # ==== Args
+  # _options_ :: オプション
+  # ==== Return
+  # 年選択セレクトボックスタグのHTML
+  # ==== Raise
+  def select_birthyear_tag(options={})
     tag = %(<select data-mini="true")
     tag << %( name="#{options[:name]}") if options[:name]
     tag << %( id="#{options[:id]}") if options[:id]
@@ -37,7 +77,8 @@ module BrowserHelper
     Rho::RhoConfig.lgdpm_max_year.to_i.times do |x|
       tag << %(<option value="#{year - x}" )
       tag << selected(year - x, options[:value])
-      tag << %(>#{year - x}</option>)
+      #tag << %(>#{year - x }</option>)
+      tag << %(>#{(year - x).to_s + "(" + to_jcalendar(year - x) + ")" }</option>)
     end
     tag << %(</select>)
   end
